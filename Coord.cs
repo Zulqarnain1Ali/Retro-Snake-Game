a084b26e143e55fe0860f0aa404d5f6b6520ee5e
Zulqarnain Ali Natiq <zulqarnain15ali@gmail.com>
2026-03-30 20:16:40 +0500

    added Coord class to handle grid positioning

---
+++ Coord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Retro_Snake_Game
+{
+    internal class Coord
+    {
+        private int x;
+        private int y;
+
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Retro_Snake_Game;
+
+Console.WriteLine("Hello, World!");
+
+Coord griddimensions = new Coord(50, 20);
+
+Coord snakePos = new Coord(10, 0);
